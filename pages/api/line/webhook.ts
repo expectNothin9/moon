b8bfcb3c731af9d00980529c137942ad02b56ec5
expectNothin9 <expect.nothin9@gmail.com>
2021-01-08 18:49:59 +0800

    fix: narrow down the issue

--- pages/api/line/webhook.ts
+++ pages/api/line/webhook.ts
@@ -99,11 +99,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
               columns: [
                 {
                   imageUrl:
-                    'https://instagram.ftpe12-1.fna.fbcdn.net/v/t51.2885-15/fr/e15/s1080x1080/133852809_823622258485938_371339567555113004_n.jpg?_nc_ht=instagram.ftpe12-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=DCCtHKKQEiYAX9rKPLN&tp=1&oh=085e665b0c878818efd3696c9ad9f599&oe=6020A3BB',
+                    'https://instagram.ftpe12-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/130117511_174890051048164_3725753661346970517_n.jpg?_nc_ht=instagram.ftpe12-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=lfmGLfe_FqgAX8NnaKb&tp=1&oh=153f8c2fdc4588a50a97d03d9a6d375a&oe=6022FC4E',
                   action: {
                     type: 'postback',
-                    label: '@nicalin0707',
-                    data: 'action=beauty-pageant&match=CJm99Punf5R,CIn0VcmH-eT&win=CJm99Punf5R'
+                    label: 'label',
+                    data: 'action=beauty-pageant&match=CJm99Punf5R,CIn0VcmH-eT&win=CIn0VcmH-eT'
                   }
                 }
               ]
@@ -123,11 +123,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
               columns: [
                 {
                   imageUrl:
-                    'https://instagram.ftpe12-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/129745327_665427261003150_1268165138661069468_n.jpg?_nc_ht=instagram.ftpe12-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=sAKnMsQHPsQAX-mWsuK&tp=1&oh=773dc84cd3a4a6438740285a631b2fc7&oe=6020C9E6',
+                    'https://instagram.ftpe12-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/130117511_174890051048164_3725753661346970517_n.jpg?_nc_ht=instagram.ftpe12-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=lfmGLfe_FqgAX8NnaKb&tp=1&oh=153f8c2fdc4588a50a97d03d9a6d375a&oe=6022FC4E',
                   action: {
                     type: 'postback',
-                    label: '@nicalin0707',
-                    data: 'action=beauty-pageant&match=CJm99Punf5R,CIn0VcmH-eT&win=CJm99Punf5R'
+                    label: '@kristina6.23',
+                    data: 'action=beauty-pageant'
                   }
                 }
               ]
@@ -150,8 +150,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                     'https://instagram.ftpe12-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/129745327_665427261003150_1268165138661069468_n.jpg?_nc_ht=instagram.ftpe12-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=sAKnMsQHPsQAX-mWsuK&tp=1&oh=773dc84cd3a4a6438740285a631b2fc7&oe=6020C9E6',
                   action: {
                     type: 'postback',
-                    label: '@u_ting221',
-                    data: 'action=beauty-pageant&match=CIj9aUrnzos,CIzcbLUHhF9&win=CIj9aUrnzos'
+                    label: '@kristina6.23',
+                    data: 'action=beauty-pageant&match=CJm99Punf5R,CIn0VcmH-eT&win=CIn0VcmH-eT'
                   }
                 }
               ]
