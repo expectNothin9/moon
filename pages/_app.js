b6106090ef715309b15fed8c39d104ff52cc655c
expectNothin9 <expect.nothin9@gmail.com>
2021-01-08 12:57:30 +0800

    feat: adopt styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
 import '../reset.css'
 
 import { Provider } from 'react-redux'
+import { createGlobalStyle } from 'styled-components'
 
 import store from '../store'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`
+
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <GlobalStyle />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   )
 }
 
