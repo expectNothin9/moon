6d3b602c80fb8d49d38a0fa8c3210442e69e9b21
expectNothin9 <expect.nothin9@gmail.com>
2021-01-22 18:06:18 +0800

    feat: integrate map with redux action

--- features/houses/housesSlices.ts
+++ features/houses/housesSlices.ts
@@ -1,9 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const LANDMARKS = [
+  {
+    id: 'TAIPEI_MAIN_STATION',
+    name: '台北車站',
+    lonLat: [121.5173735, 25.0477022]
+  },
+  { id: 'TAIPEI_101', name: '台北101', lonLat: [121.5645389, 25.033976] },
+  {
+    id: 'CKS_MEMORIAL_HALL',
+    name: '中正紀念堂',
+    lonLat: [121.521781, 25.0346119]
+  }
+]
 const initialState = {
   map: {
     initialized: false
-  }
+  },
+  landmarks: LANDMARKS,
+  selectedId: ''
 }
 
 const housesSlice = createSlice({
@@ -12,10 +27,13 @@ const housesSlice = createSlice({
   reducers: {
     setMapInitialized: (state, action) => {
       state.map.initialized = action.payload
+    },
+    setSelectedId: (state, action) => {
+      state.selectedId = action.payload
     }
   }
 })
 
-export const { setMapInitialized } = housesSlice.actions
+export const { setMapInitialized, setSelectedId } = housesSlice.actions
 
 export default housesSlice.reducer
--- pages/houses/index.tsx
+++ pages/houses/index.tsx
@@ -1,10 +1,10 @@
 import LinearProgress from '@material-ui/core/LinearProgress'
 import { withStyles } from '@material-ui/core/styles'
 import { useEffect, useRef } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
 
-import { setMapInitialized } from '../../features/houses/housesSlices'
+import { setMapInitialized, setSelectedId } from '../../features/houses/housesSlices'
 import { RootState } from '../../store'
 import { loadCss, loadJs } from '../../util/load'
 
@@ -12,6 +12,11 @@ const OL_CSS =
   'https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/css/ol.css'
 const OL_JS =
   'https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js'
+
+const DEFAULT = {
+  center: [121.5173735, 25.0477022],
+  zoom: 14
+}
 
 const StyledLinearProgress = withStyles(() => ({
   colorPrimary: {
@@ -36,16 +41,20 @@ const Loading = () => (
 
 const StyledMap = styled.section`
   width: 100%;
-  height: 60vh;
+  height: 100vh;
   background: #fff;
   position: relative;
 `
 const Map = () => {
   const map = useRef()
   const dispatch = useDispatch()
-  const mapInitialized = useSelector((state: RootState) => {
-    return state.houses.map.initialized
-  })
+  const { mapInitialized, landmarks } = useSelector((state: RootState) => {
+    const { map, landmarks } = state.houses
+    return {
+      mapInitialized: map.initialized,
+      landmarks
+    }
+  }, shallowEqual)
 
   useEffect(() => {
     if (!mapInitialized) {
@@ -59,18 +68,58 @@ const Map = () => {
           // eslint-disable-next-line
           // @ts-ignore
           const ol = window.ol
+          const { Icon, Style } = ol.style
+          const pinIconStyle = new Style({
+            image: new Icon({
+              anchor: [0.5, 0.75],
+              anchorXUnits: 'fraction',
+              anchorYUnits: 'fraction',
+              src: 'icons/star-pin.png'
+            })
+          })
+          const landmarkFeatures = landmarks.map((landmark) => {
+            const { lonLat, id } = landmark
+            const feature = new ol.Feature({
+              geometry: new ol.geom.Point(ol.proj.fromLonLat(lonLat)),
+              id
+            })
+            feature.setStyle(pinIconStyle)
+            return feature
+          })
+
+          const vectorSource = new ol.source.Vector({
+            features: landmarkFeatures
+          })
+          const vectorLayer = new ol.layer.Vector({
+            source: vectorSource
+          })
           map.current = new ol.Map({
             target: 'map',
             layers: [
               new ol.layer.Tile({
                 source: new ol.source.OSM()
-              })
+              }),
+              vectorLayer
             ],
             view: new ol.View({
-              center: ol.proj.fromLonLat([37.41, 8.82]),
-              zoom: 4
+              center: ol.proj.fromLonLat(DEFAULT.center),
+              zoom: DEFAULT.zoom
             })
           })
+
+          // eslint-disable-next-line
+          // @ts-ignore
+          map.current.on('click', (event) => {
+            // eslint-disable-next-line
+            // @ts-ignore
+            const landmark = map.current.forEachFeatureAtPixel(event.pixel, (feature) => {
+              return feature
+            })
+            if (landmark) {
+              dispatch(setSelectedId(landmark.get('id')))
+            }
+          })
+
           dispatch(setMapInitialized(true))
         } else {
           console.log('failed to initialize HousesPage')
@@ -78,18 +127,47 @@ const Map = () => {
       }
       loadResources()
     }
-  }, [dispatch, mapInitialized])
+  }, [dispatch, mapInitialized, landmarks])
   return <StyledMap id="map">{!mapInitialized && <Loading />}</StyledMap>
 }
+
+const StyledInformation = styled.section`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.8);
+  color: var(--illuminating);
+`
+const Information = () => {
+  const dispatch = useDispatch()
+  const { selectedLandmark } = useSelector((state: RootState) => {
+    const { landmarks, selectedId } = state.houses
+    return {
+      selectedLandmark: landmarks.find((landmark) => landmark.id === selectedId)
+    }
+  }, shallowEqual)
+  return selectedLandmark ? (
+    <StyledInformation onClick={() => dispatch(setSelectedId(''))}>
+      {selectedLandmark.name}
+    </StyledInformation>
+  ) : null
+}
+
 const StyledHousesPage = styled.main`
   width: 100%;
   height: 100%;
-  background: var(--ultimate-gray);
 `
 
 const HousesPage = () => (
   <StyledHousesPage>
     <Map />
+    <Information />
   </StyledHousesPage>
 )
 
